03a97a43e18512c0cd406a77c281ee96573fc644
ikostadinov542 <143066556+ikostadinov542@users.noreply.github.com>
2023-11-24 07:56:43 +0200

    Students

---
+++ Student.java
@@ -0,0 +1,33 @@
+package StudentsExercise;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String homeTown;
+
+    public Student(String firstName, String lastName, int age, String homeTown) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.homeTown = homeTown;
+    }
+
+    public String getHomeTown() {
+        return this.homeTown;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
